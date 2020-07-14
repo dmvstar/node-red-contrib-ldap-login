e058ff78f8e0f3cdc86c43335cc5cd68f13e744d
StarDust <dmvstar.devel@gmail.com>
2020-07-15 00:25:46 +0300

    Initial commit source

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# node-red-contrib-ldap-login
 LDAP user login check for Node-RED
+=========
+
+
+node-red-contrib-ldap-login is a [Node-RED](https://nodered.org/) node for Microsoft Active Directory. It is based on the [ldapjs](https://www.npmjs.com/package/ldapjs) ldapjs client for auth (authentication) and authZ (authorization) for Microsoft Active Directory.
+
+If you want to contribute in order to add brand new features and/or nodes, do not hesitate to join and submit your merge requests!
+
+Getting started
+--------------
+
+1. Install Node-RED ([more](https://nodered.org/docs/getting-started/installation)):
+```sh
+sudo npm install -g node-red
+```
+2. Go to your node-RED conf directory (basically `~/.node-red`)
+```sh
+npm install node-red-contrib-activedirectory
+```
+3. There you go! You can run Node-RED with:
+```sh
+node-red
+```
+
+Documentation
+--------------
++ [Connection](#connection)
++ [findUser](#finduser)
+
+---------------------------------------
+
+<a id="connection"></a>
+### Connection
+
+Every node requires LDAP configuration/credentials to create an instance of the client configured according to the following options:
++ `url` {string}: Active Directory server to connect to, e.g. `ldap://ad.example.com`.
++ `[baseDN]` {string}: Optional, The root DN from which all searches will be performed, e.g. `dc=example,dc=com`.
+
++ `username` {string}: An account name to check.
++ `password` {string}: Password for the given `username`.
+
+![image of node credentials](images/node_credentials.png)
+
+<a id="finduser"></a>
+### findUser
+
+![image of node finduser](images/node_finduser.png)
+
+Connects to a Microsoft Active Directory and returns the user corresponding to the username/DN set in `msg.payload`.
+
+__Inputs__
+
++ `msg.payload` {string}: the AD username of the user we want to get information. It also works with DN.
++ `msg.ad_attributes` {JSON Object}: the attributes we want to return for users and groups. By default:
+```json
+{
+  "user": [
+      "dn", "distinguishedName",
+      "userPrincipalName", "sAMAccountName", "mail",
+      "lockoutTime", "whenCreated", "pwdLastSet", "userAccountControl",
+      "employeeID", "sn", "givenName", "initials", "cn", "displayName",
+      "comment", "description", "url"
+  ],
+  "group": [
+      "dn", "cn", "description", "distinguishedName", "objectCategory"
+  ]
+}
+```
++ `msg.tlsOptions` {JSON Object}: (Optional) Additional options passed to TLS connection layer when connecting via ldaps://. (See: [TLS docs for node.js](https://nodejs.org/api/tls.html#tls_tls_connect_options_callback)).
+
+__Outputs__
+
++ `msg.payload` {JSON Object}: the standard output of the command, a JSON object that contains all the information about the user.
+
---
+++ nodes/login-user.js
@@ -0,0 +1,116 @@
+module.exports = function(RED) {
+  function loginUserNode(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+    // we get the properties
+    node.url = config.url;
+    node.baseDN = config.baseDN;
+    // we get the credentials
+    var cUsername = this.credentials.username;
+    var cPassword = this.credentials.password;
+
+    node.on("input", function(msg) {
+      node.status({
+        fill: "blue",
+        shape: "ring",
+        text: "connecting"
+      });
+      // import ldapjs
+      var ldap = require("ldapjs");
+
+      if (msg.payload.username) cUsername = msg.payload.username;
+      if (msg.payload.password) cPassword = msg.payload.password;
+
+      //console.log('['+cUsername+']['+cPassword+']');
+
+      var adConfig = {
+        url: node.url,
+        baseDN: node.baseDN,
+        username: cUsername,
+        password: cPassword
+      };
+
+      // set attributes if defined
+      if (msg.ad_attributes) {
+        // Validates the Object format (required for IBMi platform)
+        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes));
+      }
+      if (msg.tlsOptions) {
+        // Validates the Object format (required for IBMi platform)
+        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions));
+      }
+
+      try {
+        var client = ldap.createClient({
+          url: node.url,
+          timeout: 5000,
+          connectTimeout: 10000
+        });
+        node.status({
+          fill: "green",
+          shape: "dot",
+          text: "connected"
+        });
+
+        client.bind(cUsername, cPassword, function(error) {
+          //console.log('--- try to bind --- ['+username+']');
+          node.status({
+            fill: "blue",
+            shape: "ring",
+            text: "querying"
+          });
+
+          if (error) {
+            //console.log(error.message);
+            client.unbind(function(error) {
+              if (error) {
+                //console.log(error.message);} else{console.log('1 client disconnected');
+              }
+            });
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: "login error"
+            });
+            msg.payload = { user: cUsername, login: false };
+            node.send(msg);
+            //node.error('ERROR login: ' + error.message);
+          } else {
+            //console.log('connected');
+            node.status({
+              fill: "green",
+              shape: "dot",
+              text: "login"
+            });
+            client.unbind(function(error) {
+              if (error) {
+                //console.log(error.message);} else{console.log('1 client disconnected');
+              }
+            });
+            msg.payload = { user: cUsername, login: true };
+            node.send(msg);
+          }
+        });
+      } catch (e) {
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: "connexion error"
+        });
+        node.error("ERROR connecting: " + e.message);
+      }
+    });
+  }
+
+  RED.nodes.registerType("login-user", loginUserNode, {
+    credentials: {
+      username: {
+        type: "text"
+      },
+      password: {
+        type: "password"
+      }
+    }
+  });
+};
